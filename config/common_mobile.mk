9c3d573f5d2abb78254112ad6002ffea694cc469
Arseniy Graur <argraur@gmail.com>
2020-01-24 14:08:14 +0000

    vendor: Use Pixel charger animation
    Change-Id: I89ac5aa6c4dec0ded4524e94dc07fd268690b5f4
    Signed-off-by: Arseniy Graur <argraur@gmail.com>
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- config/common_mobile.mk
+++ config/common_mobile.mk
@@ -1,10 +1,6 @@
 # Inherit common mobile PixelBuilds stuff
 $(call inherit-product, vendor/standart/config/common.mk)
 
-# Charger
-PRODUCT_PACKAGES += \
-    charger_res_images
-
 # Customizations
 PRODUCT_PACKAGES += \
     NavigationBarMode2ButtonOverlay
