a6f45fbf5a420730c83006cd7fc755d19872542b
EnesSastim <sastimenes@gmail.com>
2023-11-28 20:30:48 +0100

    vendor: build Updater
    Change-Id: I3d356bebc68e19979f7a1d6be9dc9e61fe95025f

--- config/common.mk
+++ config/common.mk
@@ -172,8 +172,13 @@ PRODUCT_PACKAGES += \
 	libtextclassifier_lang_id_model
 
 # Updater
+ifeq ($(STANDARTOS_BUILD_TYPE),release)
+PRODUCT_PACKAGES += \
+    Updater
+
 PRODUCT_COPY_FILES += \
     vendor/standart/prebuilt/common/etc/init/init.standartos-updater.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.pixelbuilds-updater.rc
+endif
 
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
 
