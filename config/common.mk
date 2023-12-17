a57d0b7b8f769c0e44eef1edc27aa1e046a13b15
ExactExampl <64069095+ExactExampl@users.noreply.github.com>
2023-12-17 14:36:58 +0200

    vendor: config: include certified props overlay from StandartOS/private
    Change-Id: I5211692503ee3694f0a393fabe36c38bc0547d3a

--- config/common.mk
+++ config/common.mk
@@ -180,3 +180,4 @@ include vendor/standart/config/bootanimation.mk
 include vendor/standart/config/fonts.mk
 include vendor/standart/config/rro_overlays.mk
 include vendor/gms/products/gms.mk
+-include vendor/standart/private/certified_props.mk
