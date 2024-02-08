a4d52d60fef9bf8a309df19c5d66024efa5b3224
Stella Bloom <windowz414@gnuweeb.org>
2024-02-08 05:16:18 +0100

    config: version: Relax more properties for "overlay ROMs"
    Signed-off-by: Stella Bloom <windowz414@gnuweeb.org>

--- config/version.mk
+++ config/version.mk
@@ -5,7 +5,7 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 endif
 
 # Internal version
-STANDARTOS_VERSION := QPR1
+STANDARTOS_VERSION ?= QPR1
 STANDARTOS_BUILD_TYPE ?= unsigned
 
 # Platform version
@@ -15,7 +15,7 @@ TARGET_PRODUCT_SHORT := $(subst aosp_,,$(STANDART_BUILD))
 STANDARTOS_VERSION := StandartOS_$(TARGET_PRODUCT_SHORT)-$(PLAT_VERSION)_$(STANDARTOS_VERSION)-$(shell date +%Y%m%d-%H%M)
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.build.version.custom=$(STANDARTOS_VERSION) \
+    ro.build.version.custom?=$(STANDARTOS_VERSION) \
     ro.build.version.device=$(TARGET_PRODUCT_SHORT) \
-    ro.standart.build.version=$(STANDARTOS_VERSION) \
-    ro.standart.buildtype=$(STANDARTOS_BUILD_TYPE)
+    ro.standart.build.version?=$(STANDARTOS_VERSION) \
+    ro.standart.buildtype?=$(STANDARTOS_BUILD_TYPE)
