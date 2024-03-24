PixelBuildsROM/android_vendor_pb

vendor: config: make soong variables follow json syntax
TODO the subst does noting now so should be easier to write

wrong: CROSS_COMPILE=\\"/data4...
correct: CROSS_COMPILE=\"/data4...

Change-Id: I5080aef7feb2ec7429cb9ba1f4baccefa61a3cd3

config/BoardConfigSoong.mk
@@ -21,7 +21,7 @@ SOONG_CONFIG_pbVarsPlugin :=
 
 define addVar
   SOONG_CONFIG_pbVarsPlugin += $(1)
-  SOONG_CONFIG_pbVarsPlugin_$(1) := $$(subst ",\",$$($1))
+  SOONG_CONFIG_pbVarsPlugin_$(1) := $$(subst ",",$$($1))
 endef
 
 $(foreach v,$(EXPORT_TO_SOONG),$(eval $(call addVar,$(v))))

StandartOS/vendor_stand

config/BoardConfigSoong.mk
@@ -21,7 +21,7 @@ SOONG_CONFIG_standartVarsPlugin :=
 
 define addVar
   SOONG_CONFIG_standartVarsPlugin += $(1)
-  SOONG_CONFIG_standartVarsPlugin_$(1) := $$(subst ",\",$$($1))
+  SOONG_CONFIG_standartVarsPlugin_$(1) := $$(subst ",",$$($1))
 endef
 
 $(foreach v,$(EXPORT_TO_SOONG),$(eval $(call addVar,$(v))))