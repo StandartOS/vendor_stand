PixelBuildsROM/android_vendor_pb

vendor: introduce the initial build script
* Taken from AOSPA - https://github.com/AOSPA/android_vendor_aospa/blob/5632c764962a0533a47efaa682168bb07c9802f4/build.sh
* Simplified and modified to suit our needs

build.sh
@@ -1,0 +1,177 @@
+#!/usr/bin/env bash
+# PixelBuilds build helper script
+
+# red = errors, cyan = warnings, green = confirmations, blue = informational
+# plain for generic text, bold for titles, reset flag at each end of line
+# plain blue should not be used for readability reasons - use plain cyan instead
+CLR_RST=$(tput sgr0)                        ## reset flag
+CLR_RED=$CLR_RST$(tput setaf 1)             #  red, plain
+CLR_GRN=$CLR_RST$(tput setaf 2)             #  green, plain
+CLR_BLU=$CLR_RST$(tput setaf 4)             #  blue, plain
+CLR_CYA=$CLR_RST$(tput setaf 6)             #  cyan, plain
+CLR_BLD=$(tput bold)                        ## bold flag
+CLR_BLD_RED=$CLR_RST$CLR_BLD$(tput setaf 1) #  red, bold
+CLR_BLD_GRN=$CLR_RST$CLR_BLD$(tput setaf 2) #  green, bold
+CLR_BLD_BLU=$CLR_RST$CLR_BLD$(tput setaf 4) #  blue, bold
+CLR_BLD_CYA=$CLR_RST$CLR_BLD$(tput setaf 6) #  cyan, bold
+
+# Set defaults
+BUILD_TYPE="userdebug"
+
+function checkExit () {
+    EXIT_CODE=$?
+    if [ $EXIT_CODE -ne 0 ]; then
+        echo "${CLR_BLD_RED}Build failed!${CLR_RST}"
+        echo -e ""
+        exit $EXIT_CODE
+    fi
+}
+
+# Releaser path
+RELEASE_HELPER_PATH="vendor/pb/private/release.sh"
+
+# Output usage help
+function showHelpAndExit {
+        echo -e "${CLR_BLD_BLU}Usage: $0 <device> [options]${CLR_RST}"
+        echo -e ""
+        echo -e "${CLR_BLD_BLU}Options:${CLR_RST}"
+        echo -e "${CLR_BLD_BLU}  -h, --help            Display this help message${CLR_RST}"
+        echo -e "${CLR_BLD_BLU}  -c, --clean           Wipe the tree before building${CLR_RST}"
+        echo -e "${CLR_BLD_BLU}  -i, --installclean    Dirty build - Use 'installclean'${CLR_RST}"
+        echo -e "${CLR_BLD_BLU}  -s, --repo-sync       Sync before building${CLR_RST}"
+        echo -e "${CLR_BLD_BLU}  -r, --release         Whether we should do a signed release build"
+        echo -e "${CLR_BLD_BLU}  -t, --build-type      Specify build type${CLR_RST}"
+        echo -e "${CLR_BLD_BLU}  -j, --jobs            Specify jobs/threads to use${CLR_RST}"
+        echo -e "${CLR_BLD_BLU}  -k, --sign-keys       Specify path to sign key mappings${CLR_RST}"
+        echo -e "${CLR_BLD_BLU}  -p, --pwfile          Specify path to sign key password file${CLR_RST}"
+        exit 1
+}
+
+# Setup getopt.
+long_opts="help,clean,installclean,repo-sync,release,build-type:,jobs:,module:,sign-keys:,pwfile:,backup-unsigned,delta:,imgzip,version:"
+getopt_cmd=$(getopt -o hcris:t:j:k:p: --long "$long_opts" \
+            -n $(basename $0) -- "$@") || \
+            { echo -e "${CLR_BLD_RED}\nError: Getopt failed. Extra args\n${CLR_RST}"; showHelpAndExit; exit 1;}
+
+eval set -- "$getopt_cmd"
+
+while true; do
+    case "$1" in
+        -h|--help|h|help) showHelpAndExit;;
+        -c|--clean|c|clean) FLAG_CLEAN_BUILD=y;;
+        -i|--installclean|i|installclean) FLAG_INSTALLCLEAN_BUILD=y;;
+        -s|--repo-sync|s|repo-sync) FLAG_SYNC=y;;
+        -r|--release|r|release) FLAG_PB_RELEASE=y;;
+        -t|--build-type|t|build-type) BUILD_TYPE="$2"; shift;;
+        -j|--jobs|j|jobs) JOBS="$2"; shift;;
+        -k|--sign-keys|k|sign-keys) KEY_MAPPINGS="$2"; shift;;
+        -p|--pwfile|p|pwfile) PWFILE="$2"; shift;;
+        #-d|--delta|d|delta) DELTA_TARGET_FILES="$2"; shift;;
+        --) shift; break;;
+    esac
+    shift
+done
+
+# Mandatory argument
+if [ $# -eq 0 ]; then
+    echo -e "${CLR_BLD_RED}Error: No device specified${CLR_RST}"
+    showHelpAndExit
+fi
+export DEVICE="$1"; shift
+
+# Make sure we are running on 64-bit before carrying on with anything
+ARCH=$(uname -m | sed 's/x86_//;s/i[3-6]86/32/')
+if [ "$ARCH" != "64" ]; then
+        echo -e "${CLR_BLD_RED}error: unsupported arch (expected: 64, found: $ARCH)${CLR_RST}"
+        exit 1
+fi
+
+# Set up paths
+cd $(dirname $0)
+DIR_ROOT=$(pwd)
+
+# Make sure everything looks sane so far
+if [ ! -d "$DIR_ROOT/vendor/pb" ]; then
+        echo -e "${CLR_BLD_RED}error: insane root directory ($DIR_ROOT)${CLR_RST}"
+        exit 1
+fi
+
+# Initializationizing!
+echo -e "${CLR_BLD_BLU}Setting up the environment${CLR_RST}"
+echo -e ""
+. build/envsetup.sh
+
+# Use the thread count specified by user
+CMD=""
+if [ $JOBS ]; then
+  CMD+="-j$JOBS"
+fi
+
+# Pick the default thread count (allow overrides from the environment)
+if [ -z "$JOBS" ]; then
+        if [ "$(uname -s)" = 'Darwin' ]; then
+                JOBS=$(sysctl -n machdep.cpu.core_count)
+        else
+                JOBS=$(cat /proc/cpuinfo | grep '^processor' | wc -l)
+        fi
+fi
+
+# Grab the build version
+PIXELBUILDS_VERSION="$(cat $DIR_ROOT/vendor/pb/config/version.mk | grep 'PB_VERSION := *' | sed 's/.*= //') "
+
+# Prep for a clean build, if requested so
+if [ "$FLAG_CLEAN_BUILD" = 'y' ]; then
+        echo -e "${CLR_BLD_BLU}Cleaning output files left from old builds${CLR_RST}"
+        echo -e ""
+        m clobber "$CMD"
+fi
+
+# Sync up, if asked to
+if [ "$FLAG_SYNC" = 'y' ]; then
+        echo -e "${CLR_BLD_BLU}Downloading the latest source files${CLR_RST}"
+        echo -e ""
+        repo sync -j"$JOBS" -c --no-clone-bundle --current-branch --no-tags
+fi
+
+# Check the starting time (of the real build process)
+TIME_START=$(date +%s.%N)
+
+# Friendly logging to tell the user everything is working fine is always nice
+echo -e "${CLR_BLD_GRN}Building PixelBuilds $PIXELBUILDS_VERSION for $DEVICE${CLR_RST}"
+echo -e "${CLR_GRN}Start time: $(date)${CLR_RST}"
+echo -e ""
+
+# Lunch-time!
+echo -e "${CLR_BLD_BLU}Lunching $DEVICE${CLR_RST} ${CLR_CYA}(Including dependencies sync)${CLR_RST}"
+echo -e ""
+lunch "aosp_$DEVICE-$BUILD_TYPE"
+PIXELBUILDS_VERSION="$(get_build_var PIXELBUILDS_VERSION)"
+checkExit
+echo -e ""
+
+# Perform installclean, if requested so
+if [ "$FLAG_INSTALLCLEAN_BUILD" = 'y' ]; then
+	echo -e "${CLR_BLD_BLU}Cleaning compiled image files left from old builds${CLR_RST}"
+	echo -e ""
+	m installclean "$CMD"
+fi
+
+# Build away!
+
+# Check if we do a release build. If so, build signed release package, else - make bacon
+if [ "$FLAG_PB_RELEASE" ]; then
+        if [ -e "$RELEASE_HELPER_PATH" ]; then
+        echo -e "${CLR_BLD_BLU}Will now begin the release build${CLR_RST}"
+        source "$RELEASE_HELPER_PATH"
+        release
+        checkExit
+        else
+                echo -e "${CLR_BLD_RED}error: The release flag is set, but the helper doesn't exist !${CLR_RST}"
+                exit 1
+        fi
+else
+        echo -e "${CLR_BLD_BLU}Start making bacon${CLR_RST}"
+        echo -e ""
+        make bacon -j$JOBS
+        checkExit
+fi

StandartOS/vendor_stand

build.sh
@@ -1,0 +1,177 @@
+#!/usr/bin/env bash
+# StandartOS build helper script
+
+# red = errors, cyan = warnings, green = confirmations, blue = informational
+# plain for generic text, bold for titles, reset flag at each end of line
+# plain blue should not be used for readability reasons - use plain cyan instead
+CLR_RST=$(tput sgr0)                        ## reset flag
+CLR_RED=$CLR_RST$(tput setaf 1)             #  red, plain
+CLR_GRN=$CLR_RST$(tput setaf 2)             #  green, plain
+CLR_BLU=$CLR_RST$(tput setaf 4)             #  blue, plain
+CLR_CYA=$CLR_RST$(tput setaf 6)             #  cyan, plain
+CLR_BLD=$(tput bold)                        ## bold flag
+CLR_BLD_RED=$CLR_RST$CLR_BLD$(tput setaf 1) #  red, bold
+CLR_BLD_GRN=$CLR_RST$CLR_BLD$(tput setaf 2) #  green, bold
+CLR_BLD_BLU=$CLR_RST$CLR_BLD$(tput setaf 4) #  blue, bold
+CLR_BLD_CYA=$CLR_RST$CLR_BLD$(tput setaf 6) #  cyan, bold
+
+# Set defaults
+BUILD_TYPE="userdebug"
+
+function checkExit () {
+    EXIT_CODE=$?
+    if [ $EXIT_CODE -ne 0 ]; then
+        echo "${CLR_BLD_RED}Build failed!${CLR_RST}"
+        echo -e ""
+        exit $EXIT_CODE
+    fi
+}
+
+# Releaser path
+RELEASE_HELPER_PATH="vendor/standart/private/release.sh"
+
+# Output usage help
+function showHelpAndExit {
+        echo -e "${CLR_BLD_BLU}Usage: $0 <device> [options]${CLR_RST}"
+        echo -e ""
+        echo -e "${CLR_BLD_BLU}Options:${CLR_RST}"
+        echo -e "${CLR_BLD_BLU}  -h, --help            Display this help message${CLR_RST}"
+        echo -e "${CLR_BLD_BLU}  -c, --clean           Wipe the tree before building${CLR_RST}"
+        echo -e "${CLR_BLD_BLU}  -i, --installclean    Dirty build - Use 'installclean'${CLR_RST}"
+        echo -e "${CLR_BLD_BLU}  -s, --repo-sync       Sync before building${CLR_RST}"
+        echo -e "${CLR_BLD_BLU}  -r, --release         Whether we should do a signed release build"
+        echo -e "${CLR_BLD_BLU}  -t, --build-type      Specify build type${CLR_RST}"
+        echo -e "${CLR_BLD_BLU}  -j, --jobs            Specify jobs/threads to use${CLR_RST}"
+        echo -e "${CLR_BLD_BLU}  -k, --sign-keys       Specify path to sign key mappings${CLR_RST}"
+        echo -e "${CLR_BLD_BLU}  -p, --pwfile          Specify path to sign key password file${CLR_RST}"
+        exit 1
+}
+
+# Setup getopt.
+long_opts="help,clean,installclean,repo-sync,release,build-type:,jobs:,module:,sign-keys:,pwfile:,backup-unsigned,delta:,imgzip,version:"
+getopt_cmd=$(getopt -o hcris:t:j:k:p: --long "$long_opts" \
+            -n $(basename $0) -- "$@") || \
+            { echo -e "${CLR_BLD_RED}\nError: Getopt failed. Extra args\n${CLR_RST}"; showHelpAndExit; exit 1;}
+
+eval set -- "$getopt_cmd"
+
+while true; do
+    case "$1" in
+        -h|--help|h|help) showHelpAndExit;;
+        -c|--clean|c|clean) FLAG_CLEAN_BUILD=y;;
+        -i|--installclean|i|installclean) FLAG_INSTALLCLEAN_BUILD=y;;
+        -s|--repo-sync|s|repo-sync) FLAG_SYNC=y;;
+        -r|--release|r|release) FLAG_STANDARTOS_RELEASE=y;;
+        -t|--build-type|t|build-type) BUILD_TYPE="$2"; shift;;
+        -j|--jobs|j|jobs) JOBS="$2"; shift;;
+        -k|--sign-keys|k|sign-keys) KEY_MAPPINGS="$2"; shift;;
+        -p|--pwfile|p|pwfile) PWFILE="$2"; shift;;
+        #-d|--delta|d|delta) DELTA_TARGET_FILES="$2"; shift;;
+        --) shift; break;;
+    esac
+    shift
+done
+
+# Mandatory argument
+if [ $# -eq 0 ]; then
+    echo -e "${CLR_BLD_RED}Error: No device specified${CLR_RST}"
+    showHelpAndExit
+fi
+export DEVICE="$1"; shift
+
+# Make sure we are running on 64-bit before carrying on with anything
+ARCH=$(uname -m | sed 's/x86_//;s/i[3-6]86/32/')
+if [ "$ARCH" != "64" ]; then
+        echo -e "${CLR_BLD_RED}error: unsupported arch (expected: 64, found: $ARCH)${CLR_RST}"
+        exit 1
+fi
+
+# Set up paths
+cd $(dirname $0)
+DIR_ROOT=$(pwd)
+
+# Make sure everything looks sane so far
+if [ ! -d "$DIR_ROOT/vendor/standart" ]; then
+        echo -e "${CLR_BLD_RED}error: insane root directory ($DIR_ROOT)${CLR_RST}"
+        exit 1
+fi
+
+# Initializationizing!
+echo -e "${CLR_BLD_BLU}Setting up the environment${CLR_RST}"
+echo -e ""
+. build/envsetup.sh
+
+# Use the thread count specified by user
+CMD=""
+if [ $JOBS ]; then
+  CMD+="-j$JOBS"
+fi
+
+# Pick the default thread count (allow overrides from the environment)
+if [ -z "$JOBS" ]; then
+        if [ "$(uname -s)" = 'Darwin' ]; then
+                JOBS=$(sysctl -n machdep.cpu.core_count)
+        else
+                JOBS=$(cat /proc/cpuinfo | grep '^processor' | wc -l)
+        fi
+fi
+
+# Grab the build version
+PIXELBUILDS_VERSION="$(cat $DIR_ROOT/vendor/standart/config/version.mk | grep 'STANDARTOS_VERSION := *' | sed 's/.*= //') "
+
+# Prep for a clean build, if requested so
+if [ "$FLAG_CLEAN_BUILD" = 'y' ]; then
+        echo -e "${CLR_BLD_BLU}Cleaning output files left from old builds${CLR_RST}"
+        echo -e ""
+        m clobber "$CMD"
+fi
+
+# Sync up, if asked to
+if [ "$FLAG_SYNC" = 'y' ]; then
+        echo -e "${CLR_BLD_BLU}Downloading the latest source files${CLR_RST}"
+        echo -e ""
+        repo sync -j"$JOBS" -c --no-clone-bundle --current-branch --no-tags
+fi
+
+# Check the starting time (of the real build process)
+TIME_START=$(date +%s.%N)
+
+# Friendly logging to tell the user everything is working fine is always nice
+echo -e "${CLR_BLD_GRN}Building PixelBuilds $PIXELBUILDS_VERSION for $DEVICE${CLR_RST}"
+echo -e "${CLR_GRN}Start time: $(date)${CLR_RST}"
+echo -e ""
+
+# Lunch-time!
+echo -e "${CLR_BLD_BLU}Lunching $DEVICE${CLR_RST} ${CLR_CYA}(Including dependencies sync)${CLR_RST}"
+echo -e ""
+lunch "aosp_$DEVICE-$BUILD_TYPE"
+PIXELBUILDS_VERSION="$(get_build_var PIXELBUILDS_VERSION)"
+checkExit
+echo -e ""
+
+# Perform installclean, if requested so
+if [ "$FLAG_INSTALLCLEAN_BUILD" = 'y' ]; then
+	echo -e "${CLR_BLD_BLU}Cleaning compiled image files left from old builds${CLR_RST}"
+	echo -e ""
+	m installclean "$CMD"
+fi
+
+# Build away!
+
+# Check if we do a release build. If so, build signed release package, else - make bacon
+if [ "$FLAG_STANDARTOS_RELEASE" ]; then
+        if [ -e "$RELEASE_HELPER_PATH" ]; then
+        echo -e "${CLR_BLD_BLU}Will now begin the release build${CLR_RST}"
+        source "$RELEASE_HELPER_PATH"
+        release
+        checkExit
+        else
+                echo -e "${CLR_BLD_RED}error: The release flag is set, but the helper doesn't exist !${CLR_RST}"
+                exit 1
+        fi
+else
+        echo -e "${CLR_BLD_BLU}Start making bacon${CLR_RST}"
+        echo -e ""
+        make bacon -j$JOBS
+        checkExit
+fi