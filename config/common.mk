PixelBuilds-Staging/android_vendor_pb

vendor: overlay: do not enforce RRO for common overlays

config/common.mk
@@ -150,10 +150,9 @@ PRODUCT_DEXPREOPT_SPEED_APPS += \
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     dalvik.vm.systemuicompilerfilter=speed
 
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/pb/overlay/no-rro
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/pb/overlay
 PRODUCT_PACKAGE_OVERLAYS += \
-    vendor/pb/overlay/common \
-    vendor/pb/overlay/no-rro
+    vendor/pb/overlay/common
 
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
 

StandartOS/vendor_stand

config/common.mk
@@ -150,10 +150,9 @@ PRODUCT_DEXPREOPT_SPEED_APPS += \
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     dalvik.vm.systemuicompilerfilter=speed
 
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/standart/overlay/no-rro
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/standart/overlay
 PRODUCT_PACKAGE_OVERLAYS += \
-    vendor/standart/overlay/common \
-    vendor/standart/overlay/no-rro
+    vendor/standart/overlay/common
 
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
 